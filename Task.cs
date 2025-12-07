5b55605dadaa1bcc242b559c5bf95a8e61001ca7
B H <83563712+hbinyomin@users.noreply.github.com>
2025-12-07 17:30:06 -0500

    Create Task entity

--- Task.cs
+++ Task.cs
@@ -4,6 +4,21 @@ using System.Text;
 
 namespace TaskManagmentSystem {
     internal class Task {
+        public int Id {
+            get; set;
+        }
+        public string Title { get; set; } =null!;
+        public string? Description {
+            get; set;
+        }
+        public DateTime? DueDate {
+            get; set;
+        }
+        public int PriorityLevel {
+            get; set;
+        }
 
+        public Task() {
+        }
     }
 }
