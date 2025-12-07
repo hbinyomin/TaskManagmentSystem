b10043e0cc6e50a68231ac1f871578e1c98d1bc1
B H <83563712+hbinyomin@users.noreply.github.com>
2025-12-07 17:31:07 -0500

    Add DbContext

---
+++ DbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagmentSystem {
+    internal class TaskDbContext : DbContext {
+        public DbSet<Task> Tasks { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=TASKMANAGEMENTSYSTEM;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
