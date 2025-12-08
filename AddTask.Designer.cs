2a3fe3ee2a48dfdce54812ecddaff91a961bb271
B H <83563712+hbinyomin@users.noreply.github.com>
2025-12-07 23:39:04 -0500

    Reszie Add Task form

--- AddTask.Designer.cs
+++ AddTask.Designer.cs
@@ -37,7 +37,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(80, 60);
+            label2.Location = new Point(39, 24);
             label2.Name = "label2";
             label2.Size = new Size(56, 15);
             label2.TabIndex = 5;
@@ -45,9 +45,9 @@
             // 
             // addTaskButton
             // 
-            addTaskButton.Location = new Point(80, 277);
+            addTaskButton.Location = new Point(117, 244);
             addTaskButton.Name = "addTaskButton";
-            addTaskButton.Size = new Size(127, 36);
+            addTaskButton.Size = new Size(127, 27);
             addTaskButton.TabIndex = 3;
             addTaskButton.Text = "Add task";
             addTaskButton.UseVisualStyleBackColor = true;
@@ -55,14 +55,14 @@
             // 
             // titleTextBox
             // 
-            titleTextBox.Location = new Point(80, 78);
+            titleTextBox.Location = new Point(39, 42);
             titleTextBox.Name = "titleTextBox";
             titleTextBox.Size = new Size(205, 23);
             titleTextBox.TabIndex = 6;
             // 
             // descriptionTextBox
             // 
-            descriptionTextBox.Location = new Point(82, 128);
+            descriptionTextBox.Location = new Point(41, 92);
             descriptionTextBox.Name = "descriptionTextBox";
             descriptionTextBox.Size = new Size(203, 23);
             descriptionTextBox.TabIndex = 7;
@@ -70,7 +70,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(81, 111);
+            label3.Location = new Point(40, 75);
             label3.Name = "label3";
             label3.Size = new Size(67, 15);
             label3.TabIndex = 8;
@@ -79,7 +79,7 @@
             // priorityLabel
             // 
             priorityLabel.AutoSize = true;
-            priorityLabel.Location = new Point(85, 166);
+            priorityLabel.Location = new Point(44, 130);
             priorityLabel.Name = "priorityLabel";
             priorityLabel.Size = new Size(45, 15);
             priorityLabel.TabIndex = 9;
@@ -87,7 +87,7 @@
             // 
             // dueDatePicker1
             // 
-            dueDatePicker1.Location = new Point(85, 237);
+            dueDatePicker1.Location = new Point(44, 201);
             dueDatePicker1.Name = "dueDatePicker1";
             dueDatePicker1.Size = new Size(200, 23);
             dueDatePicker1.TabIndex = 10;
@@ -95,7 +95,7 @@
             // dueDateLabel
             // 
             dueDateLabel.AutoSize = true;
-            dueDateLabel.Location = new Point(85, 219);
+            dueDateLabel.Location = new Point(44, 183);
             dueDateLabel.Name = "dueDateLabel";
             dueDateLabel.Size = new Size(55, 15);
             dueDateLabel.TabIndex = 11;
@@ -106,7 +106,7 @@
             priorityComboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             priorityComboBox1.FormattingEnabled = true;
             priorityComboBox1.Items.AddRange(new object[] { "Low", "Medium", "High", "Critical" });
-            priorityComboBox1.Location = new Point(85, 184);
+            priorityComboBox1.Location = new Point(44, 148);
             priorityComboBox1.Name = "priorityComboBox1";
             priorityComboBox1.Size = new Size(200, 23);
             priorityComboBox1.TabIndex = 12;
@@ -115,7 +115,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(407, 450);
+            ClientSize = new Size(294, 304);
             Controls.Add(priorityComboBox1);
             Controls.Add(dueDateLabel);
             Controls.Add(dueDatePicker1);
